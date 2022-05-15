d1f3e4c580c55d96ad5b86c37635594189a73d43
Nereo López <nereo.lopez@uabc.edu.mx>
2022-05-15 00:23:10 -0700

    Update 4
    Updated strings
    Updated the BeerActivity screen
    Removed the spinner and created buttons.

--- BeerBuddy/app/src/main/java/com/hfad/beerbuddy/BeerActivity.java
+++ BeerBuddy/app/src/main/java/com/hfad/beerbuddy/BeerActivity.java
@@ -18,26 +18,5 @@ public class BeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer);
-
-        state = (TextView) findViewById(R.id.spinnerState);
-        beerSelection = (Spinner) findViewById(R.id.beerSpinner);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.beerTypes, android.R.layout.simple_spinner_item);
-
-        beerSelection.setAdapter(adapter);
-
-        beerSelection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
     }
 }
